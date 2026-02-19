0fd194babe60337ad7f84c4be71027e42d2b0574
ikorason <irfanabliz914@gmail.com>
2026-02-19 12:50:41 -0500

    chore: update

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,29 @@
 return {
   -- "rebelot/kanagawa.nvim",
 
-  -- {
-  --   "neanias/everforest-nvim",
-  --   version = false,
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("everforest").setup({
-  --       italics = true,
-  --       diagnostic_virtual_text = "grey",
-  --       diagnostic_line_highlight = true,
-  --       -- on_highlights = function(hl, palette)
-  --       --   hl.TSDanger = {}
-  --       -- end,
-  --       -- on_highlights = function(hl, palette)
-  --       --   hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
-  --       --   hl.DiagnosticWarn = { fg = palette.none, bg = palette.none, sp = palette.yellow }
-  --       --   hl.DiagnosticInfo = { fg = palette.none, bg = palette.none, sp = palette.blue }
-  --       --   hl.DiagnosticHint = { fg = palette.none, bg = palette.none, sp = palette.green }
-  --       -- end,
-  --     })
-  --   end,
-  -- },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        italics = true,
+        diagnostic_virtual_text = "grey",
+        diagnostic_line_highlight = true,
+        -- on_highlights = function(hl, palette)
+        --   hl.TSDanger = {}
+        -- end,
+        -- on_highlights = function(hl, palette)
+        --   hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
+        --   hl.DiagnosticWarn = { fg = palette.none, bg = palette.none, sp = palette.yellow }
+        --   hl.DiagnosticInfo = { fg = palette.none, bg = palette.none, sp = palette.blue }
+        --   hl.DiagnosticHint = { fg = palette.none, bg = palette.none, sp = palette.green }
+        -- end,
+      })
+    end,
+  },
 
   -- {
   --   "rose-pine/neovim",
@@ -50,17 +51,17 @@ return {
   --   end,
   -- },
 
-  {
-    "sainnhe/everforest",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_background = "hard"
-    end,
-  },
+  -- {
+  --   "sainnhe/everforest",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.everforest_enable_italic = true
+  --     vim.g.everforest_background = "hard"
+  --   end,
+  -- },
 
   {
     "LazyVim/LazyVim",
